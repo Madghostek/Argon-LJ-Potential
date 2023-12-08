eb6d040420dd053dddd899847e036f42033a6b67
Madghostek <madghostek@gmail.com>
2023-12-08 20:35:15 +0100

    particles with elastic collision, but no LJ force

--- argon.js
+++ argon.js
@@ -2,7 +2,8 @@ let particles = []
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
-  initRandomParticles(10);
+  initRandomParticles(20, 5);
+  //initTest()
 }
 
 function windowResized() {
@@ -11,24 +12,37 @@ function windowResized() {
 
 function draw() {
   background(220);
+  update()
   for (p of particles){
     p.draw();
   }
 }
 
 function update(){
+  for (p of particles){
+    p.update()
+  }
   for (let i = 0; i < particles.length - 1; i++) {
     for (let j = i + 1; j < particles.length; j++) {
-      particles[i].update(particles[j]);
+      particles[i].interact(particles[j]);
     }
   }
 }
 
 // non-p5js stuff
 
-function initRandomParticles(count){
-  particles = []
-  for (let i=0; i<=count;i++){
-    particles.push(new Particle())
+function initRandomParticles(count, temperature){
+  for (let i=0; i<count;i++){
+    particles.push(new Particle(temperature))
   }
+}
+
+function initTest()
+{
+  particles = [new Particle(), new Particle()]
+  particles[0].pos = createVector(100,100)
+  particles[1].pos = createVector(500,100)
+  
+  particles[0].vel = createVector(2,0)
+  particles[1].vel = createVector(0,0)
 }
--- particle.js
+++ particle.js
@@ -1,17 +1,58 @@
 class Particle {
-	constructor()
+	constructor(temperature)
 	{
-		this.x = Math.random()*windowWidth
-		this.y = Math.random()*windowHeight
-		this.vel = createVector(Math.random(),Math.random());
+		this.pos = createVector(Math.random()*windowWidth,Math.random()*windowHeight)
+		this.vel = createVector(randomGaussian(0,temperature),randomGaussian(0,temperature));
+		this.r = 50;
 	}
 
-	update(other){
+	update(){
+		// applies velocity
+		this.pos.add(this.vel)
+
+		// teleport if off-screen
+		if (this.pos.x+this.r<0){
+			this.pos.x+=windowWidth+this.r*2
+		}
+		if (this.pos.x-this.r>windowWidth)
+			this.pos.x-=windowWidth+this.r*2
+
+		if (this.pos.y+this.r<0)
+			this.pos.y+=windowHeight+this.r*2
+		
+		if (this.pos.y-this.r>windowHeight)
+			this.pos.y-=windowHeight+this.r*2
+	}
+
+	interact(other){
 		// handles collision and LJ forces
 
+		//Collision
+		if (this.pos.dist(other.pos)<=this.r*2){
+
+
+			// get normal vector from this to other
+			const norm = p5.Vector.sub(this.pos,other.pos).div(this.pos.dist(other.pos))
+
+			//space them away to avoid bugs
+			//find midpoint of particles
+			const mid = p5.Vector.add(this.pos,other.pos).div(2)
+			print(this.pos,other.pos,mid)
+
+			// final points are radius away from midpoint, in other directions
+			this.pos=p5.Vector.add(mid,p5.Vector.mult(norm,this.r))
+			other.pos=p5.Vector.sub(mid,p5.Vector.mult(norm,this.r))
+
+			// math
+			const p = p5.Vector.dot(this.vel,norm)-p5.Vector.dot(other.vel,norm)
+
+			norm.mult(p)
+			this.vel.sub(norm)
+			other.vel.add(norm)
+		}
 	}
 
 	draw(){
-		circle(this.x,this.y,10)
+		circle(this.pos.x,this.pos.y,this.r*2)
 	}
 }
